5c49cdff303419a9d466890d10ef53359467863a
Adityakumarjha <139031943+Adityakumarjha087@users.noreply.github.com>
2025-02-26 09:43:56 +0000

    again

--- basic.cpp
+++ basic.cpp
@@ -9,6 +9,32 @@ int main(){
     int a;
     cin >> a;
     cout << a << endl;
+    /* using namespace std; → Allows using cout instead of std::cout.
+    main() → Entry point of the program.
+    cout << "Hello, World!"; → Prints output to the console.
+    return 0; → Ends the program successfully.
+    */
+   // datatype
+   int age = 21;  
+    cout << "Age: " << age << endl;  
 
-    
+    // Floating-point data type  
+    float height = 5.9;  
+    cout << "Height: " << height << " feet" << endl;  
+
+    // Double data type (more precision than float)  
+    double pi = 3.1415926535;  
+    cout << "Value of Pi: " << pi << endl;  
+
+    // Character data type  
+    char grade = 'A';  
+    cout << "Grade: " << grade << endl;  
+
+    // Boolean data type  
+    bool isStudent = true;  
+    cout << "Is Student? " << isStudent << endl;  
+
+    // String data type  
+    string name = "John Doe";  
+    cout << "Name: " << name << endl; 
 }
